2965eadd82539566b3df1ada41d70162d58216cb
AxelBroeglin <axelbroeglin@gmail.com>
2022-06-30 22:14:47 +0100

    Work on function + comment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,18 @@
 const removeFromArray = function(array, removedNumber) {
-    for( let i = 0; i < array.length; i++){ 
     
-        if ( array[i] === removedNumber) { 
+    //Créer un array des valeurs après array puis boucle forEach (puis for ?) pour comparer avec array de base.
+    let arr = Array.from(removedNumber)
+    arr.forEach(element => {
+        for( let i = 0; i < array.length; i++){ 
     
-            array.splice(i, 1); 
-            return array
+            if ( array[i] === removedNumber) { 
+                array.splice(i, 1); 
+                return array
+            }
         }
-    }
+});
+
+   
 }
 // Do not edit below this line
 module.exports = removeFromArray;
