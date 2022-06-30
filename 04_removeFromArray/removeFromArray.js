13a976bf29d67b80f870a8a2b7a6859716604034
AxelBroeglin <axelbroeglin@gmail.com>
2022-06-30 21:41:51 +0100

    1 test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,10 @@
 const removeFromArray = function(array, removedNumber) {
-    for( let i = 0; i < arr.length; i++){ 
+    for( let i = 0; i < array.length; i++){ 
     
         if ( array[i] === removedNumber) { 
     
             array.splice(i, 1); 
+            return array
         }
     }
 }
