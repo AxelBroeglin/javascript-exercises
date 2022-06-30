54ec6f04c98ad35477c121d70bd35a2e0cf39565
AxelBroeglin <axelbroeglin@gmail.com>
2022-06-30 21:38:51 +0100

    work on function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, removedNumber) {
+    for( let i = 0; i < arr.length; i++){ 
+    
+        if ( array[i] === removedNumber) { 
+    
+            array.splice(i, 1); 
+        }
+    }
+}
 // Do not edit below this line
 module.exports = removeFromArray;
