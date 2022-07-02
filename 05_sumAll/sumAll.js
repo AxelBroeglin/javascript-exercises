96cbd82d28e6d6659bc2fc932cea4b798f13bbbd
AxelBroeglin <axelbroeglin@gmail.com>
2022-07-02 18:13:04 +0100

    Function passed test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(n,m) {
+    if (n<0 || m<0 || typeof(m) !== 'number' || typeof(n) !== 'number'){
+        return 'ERROR'
+        }else {
+        if(n<m){
+        n1 = n
+        n2 = m 
+        }else{
+            n1 = m
+            n2 = n
+        }
+        return (n1+n2)*(n2/2)}
 };
 
 // Do not edit below this line
